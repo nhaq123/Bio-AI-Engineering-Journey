8dfd2348817404216c279b861dd0b5443a83aeaf
nhaq123 <nhaq7065@gmail.com>
2026-02-06 21:44:49 +0530

    creared day 05

---
+++ Month-01-Python-Foundation/Angelina yu course work/week 1 logics  and mutations/Day-05-Password generator/main.py
@@ -0,0 +1,20 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']  
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+'] 
+nr_letters = input ( "How many letters would you like in your password\n") 
+nr_symbols = input ("How many symbols would you like in your password\n")
+nr_numbers = input ("How many numbers would you like in your password\n")
+import random
+#easy level
+
+#hard level
+password_list = []
+for i in range(0,int(nr_letters)):
+    password_list.append(random.choice(letters))
+for i in range(0,int(nr_symbols)):
+    password_list.append(random.choice(symbols))
+for i in range(0,int(nr_numbers)):
+    password_list.append(random.choice(numbers))
+random.shuffle(password_list)
+print(password_list)
+      
