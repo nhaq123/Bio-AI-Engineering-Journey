1441277aad85a4e4f946fb2820f0464bc975c666
nhaq123 <nhaq7065@gmail.com>
2026-02-08 19:39:18 +0530

    Update main.py

--- Month-01-Python-Foundation/Angelina yu course work/week 1 logics  and mutations/Day-05-Password generator/main.py
+++ Month-01-Python-Foundation/Angelina yu course work/week 1 logics  and mutations/Day-05-Password generator/main.py
@@ -23,9 +23,9 @@ for i in range(0,int(nr_symbols)):
 for i in range(0,int(nr_numbers)):
     password_list.append(random.choice(numbers))
 random.shuffle(password_list)
-password = "".join(password_list) ''' or use password = ""
-                                               for char in password_list:
-                                                  password+=char '''
+password = "".join(password_list) # or use password =
+ #                                              for char in password_list:
+ #                                                 password+=char '''
                                              
 
 print(f"Your password is: {password}")
